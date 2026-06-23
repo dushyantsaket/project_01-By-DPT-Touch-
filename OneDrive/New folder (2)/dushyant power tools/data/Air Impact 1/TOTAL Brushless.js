14d1cb429bbb276226641261a385f28fe72dff50
Dushyant Saket <168100706+dushyantsaket@users.noreply.github.com>
2026-06-23 10:36:20 +0530

    Create TOTAL Brushless.js

---
+++ OneDrive/New folder (2)/dushyant power tools/data/Air Impact 1/TOTAL Brushless.js
@@ -0,0 +1,288 @@
+```json
+{
+  "category": "New Arrivals",
+  "show_all_link": "/products/New-Arrivals-c120532797",
+  "products": [
+    {
+      "id": "828970865",
+      "name": "TOTAL Professional Tool Kit 166 Pcs - THKTHP41667",
+      "url": "/products/TOTAL-Professional-Tool-Kit-166-Pcs-THKTHP41667-p828970865",
+      "label": "On sale",
+      "price": {
+        "compare": "₹13 699.00",
+        "current": "₹10 959.00"
+      },
+      "images": {
+        "primary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828970865/5706305819.png",
+        "secondary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828970865/5706304528.png"
+      }
+    },
+    {
+      "id": "828991050",
+      "name": "TOTAL Brushless Impact Wrench 1200Nm - TIWLI20105",
+      "url": "/products/TOTAL-Brushless-Impact-Wrench-1200Nm-TIWLI20105-p828991050",
+      "label": "NEW ARRIVAL",
+      "price": {
+        "compare": "₹30 599.00",
+        "current": "₹24 479.00"
+      },
+      "images": {
+        "primary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828991050/5706305269.png",
+        "secondary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828991050/5706304469.jpg"
+      }
+    },
+    {
+      "id": "828970933",
+      "name": "TOTAL Cordless Car Polisher 20V - TAPLI20151",
+      "url": "/products/TOTAL-Cordless-Car-Polisher-20V-TAPLI20151-p828970933",
+      "label": "NEW ARRIVAL",
+      "price": {
+        "compare": "₹18 499.00",
+        "current": "₹14 799.00"
+      },
+      "images": {
+        "primary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828970933/5706305755.png",
+        "secondary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828970933/5706305843.jpg"
+      }
+    },
+    {
+      "id": "828935493",
+      "name": "TOTAL Cordless Demolition Breaker 20V - TDBLI203582",
+      "url": "/products/TOTAL-Cordless-Demolition-Breaker-20V-TDBLI203582-p828935493",
+      "label": "NEW ARRIVAL",
+      "price": {
+        "compare": "₹28 665.00",
+        "current": "₹22 932.00"
+      },
+      "images": {
+        "primary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828935493/5706304905.png",
+        "secondary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828935493/5706304847.jpg"
+      }
+    },
+    {
+      "id": "828970934",
+      "name": "TOTAL Cordless Grass Trimmer 20V - TGTLI20328",
+      "url": "/products/TOTAL-Cordless-Grass-Trimmer-20V-TGTLI20328-p828970934",
+      "label": "NEW ARRIVAL",
+      "price": {
+        "compare": "₹6 070.00",
+        "current": "₹4 856.00"
+      },
+      "images": {
+        "primary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828970934/5706304765.png",
+        "secondary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828970934/5706305549.jpg"
+      }
+    },
+    {
+      "id": "828991051",
+      "name": "TOTAL Cordless String Trimmer & Brush Cutter 40V - TSTLI200285",
+      "url": "/products/TOTAL-Cordless-String-Trimmer-&-Brush-Cutter-40V-TSTLI200285-p828991051",
+      "label": "NEW ARRIVAL",
+      "price": {
+        "compare": "₹33 199.00",
+        "current": "₹26 559.00"
+      },
+      "images": {
+        "primary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828991051/5706305262.png",
+        "secondary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828991051/5706304458.jpg"
+      }
+    },
+    {
+      "id": "828935495",
+      "name": "TOTAL Brushless Air Blower 20V - TABLI204282",
+      "url": "/products/TOTAL-Brushless-Air-Blower-20V-TABLI204282-p828935495",
+      "label": "On sale",
+      "price": {
+        "compare": "₹15 550.00",
+        "current": "₹12 440.00"
+      },
+      "images": {
+        "primary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828935495/5706305235.png",
+        "secondary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828935495/5706305016.jpg"
+      }
+    },
+    {
+      "id": "828991052",
+      "name": "TOTAL P20S Multi Battery Charger 20V 6 Slot - TFCLI2064",
+      "url": "/products/TOTAL-P20S-Multi-Battery-Charger-20V-6-Slot-TFCLI2064-p828991052",
+      "label": "On sale",
+      "price": {
+        "compare": "₹5 999.00",
+        "current": "₹4 799.00"
+      },
+      "images": {
+        "primary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828991052/5706304929.png",
+        "secondary": null
+      }
+    },
+    {
+      "id": "828991053",
+      "name": "TOTAL Brushless Impact Drill Kit 42V - TIDLI426981",
+      "url": "/products/TOTAL-Brushless-Impact-Drill-Kit-42V-TIDLI426981-p828991053",
+      "label": "On sale",
+      "price": {
+        "compare": "₹14 099.00",
+        "current": "₹11 279.00"
+      },
+      "images": {
+        "primary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828991053/5706305789.png",
+        "secondary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828991053/5706305146.jpg"
+      }
+    },
+    {
+      "id": "828991054",
+      "name": "TOTAL Brushless Impact Drill Kit 42V Dual Battery - TIDLI426982",
+      "url": "/products/TOTAL-Brushless-Impact-Drill-Kit-42V-Dual-Battery-TIDLI426982-p828991054",
+      "label": "On sale",
+      "price": {
+        "compare": "₹18 149.00",
+        "current": "₹14 519.00"
+      },
+      "images": {
+        "primary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828991054/5706305807.png",
+        "secondary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828991054/5706304620.jpg"
+      }
+    },
+    {
+      "id": "828991055",
+      "name": "TOTAL Brushless Impact Drill 42V 99Nm - TIDLI429982",
+      "url": "/products/TOTAL-Brushless-Impact-Drill-42V-99Nm-TIDLI429982-p828991055",
+      "label": "On sale",
+      "price": {
+        "compare": "₹21 550.00",
+        "current": "₹17 240.00"
+      },
+      "images": {
+        "primary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828991055/5706305568.png",
+        "secondary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828991055/5706304995.jpg"
+      }
+    },
+    {
+      "id": "828935496",
+      "name": "TOTAL Brushless Impact Wrench 600Nm - TIWLI42462",
+      "url": "/products/TOTAL-Brushless-Impact-Wrench-600Nm-TIWLI42462-p828935496",
+      "label": "On sale",
+      "price": {
+        "compare": "₹19 775.00",
+        "current": "₹15 820.00"
+      },
+      "images": {
+        "primary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828935496/5706305609.png",
+        "secondary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828935496/5706296707.jpg"
+      }
+    },
+    {
+      "id": "828970935",
+      "name": "TOTAL Brushless Angle Grinder Kit 42V - TAGLI4128102",
+      "url": "/products/TOTAL-Brushless-Angle-Grinder-Kit-42V-TAGLI4128102-p828970935",
+      "label": "On sale",
+      "price": {
+        "compare": "₹22 149.00",
+        "current": "₹17 719.00"
+      },
+      "images": {
+        "primary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828970935/5706305663.png",
+        "secondary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828970935/5706304911.png"
+      }
+    },
+    {
+      "id": "828991056",
+      "name": "TOTAL Brushless Rotary Hammer Kit 42V - TRHLI422682",
+      "url": "/products/TOTAL-Brushless-Rotary-Hammer-Kit-42V-TRHLI422682-p828991056",
+      "label": "On sale",
+      "price": {
+        "compare": "₹22 360.00",
+        "current": "₹17 888.00"
+      },
+      "images": {
+        "primary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828991056/5706305187.png",
+        "secondary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828991056/5706304663.jpg"
+      }
+    },
+    {
+      "id": "828935497",
+      "name": "TOTAL Brushless Rotary Hammer 42V - TRHLI422882",
+      "url": "/products/TOTAL-Brushless-Rotary-Hammer-42V-TRHLI422882-p828935497",
+      "label": "On sale",
+      "price": {
+        "compare": "₹29 475.00",
+        "current": "₹23 580.00"
+      },
+      "images": {
+        "primary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828935497/5706305597.png",
+        "secondary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828935497/5706304899.jpg"
+      }
+    },
+    {
+      "id": "828970936",
+      "name": "TOTAL Compact Brushless Drill Driver 16V - TIDLI166062",
+      "url": "/products/TOTAL-Compact-Brushless-Drill-Driver-16V-TIDLI166062-p828970936",
+      "label": "On sale",
+      "price": {
+        "compare": "₹9 075.00",
+        "current": "₹7 260.00"
+      },
+      "images": {
+        "primary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828970936/5706305223.png",
+        "secondary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828970936/5706305031.jpg"
+      }
+    },
+    {
+      "id": "828970937",
+      "name": "TOTAL 16V Lithium-Ion Battery Pack 2.0Ah - TFBLI1620",
+      "url": "/products/TOTAL-16V-Lithium-Ion-Battery-Pack-2.0Ah-TFBLI1620-p828970937",
+      "label": "On sale",
+      "price": {
+        "compare": "₹2 250.00",
+        "current": "₹1 800.00"
+      },
+      "images": {
+        "primary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828970937/5706305274.png",
+        "secondary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828970937/5706304404.png"
+      }
+    },
+    {
+      "id": "828935500",
+      "name": "TOTAL M16 Battery Charger 16V - TFCLI1613",
+      "url": "/products/TOTAL-M16-Battery-Charger-16V-TFCLI1613-p828935500",
+      "label": "On sale",
+      "price": {
+        "compare": "₹1 130.00",
+        "current": "₹904.00"
+      },
+      "images": {
+        "primary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828935500/5706305170.png",
+        "secondary": null
+      }
+    },
+    {
+      "id": "828970941",
+      "name": "TOTAL Cordless Portable Lamp 4V - TRLF4415",
+      "url": "/products/TOTAL-Cordless-Portable-Lamp-4V-TRLF4415-p828970941",
+      "label": "On sale",
+      "price": {
+        "compare": "₹5 199.00",
+        "current": "₹4 159.00"
+      },
+      "images": {
+        "primary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828970941/5706305801.png",
+        "secondary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828970941/5706305182.png"
+      }
+    },
+    {
+      "id": "828995253",
+      "name": "TOTAL Sliding Mitre Saw 1800W - TS421825517",
+      "url": "/products/TOTAL-Sliding-Mitre-Saw-1800W-TS421825517-p828995253",
+      "label": "NEW ARRIVAL",
+      "price": {
+        "compare": "₹28 899.00",
+        "current": "₹23 119.00"
+      },
+      "images": {
+        "primary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828995253/5706305199.png",
+        "secondary": "https://d2j6dbq0eux0bg.cloudfront.net/images/62425538/products/828995253/5706304523.png"
+      }
+    }
+  ]
+}
+```
