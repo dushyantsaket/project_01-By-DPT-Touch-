04494a1410f152b107a0b49b37b62041e816d3df
Dushyant Saket <168100706+dushyantsaket@users.noreply.github.com>
2026-06-24 11:32:28 +0530

    Create JcbProduts.js

---
+++ OneDrive/New folder (2)/dushyant power tools/data/Air Impact 1/JcbProduts.js
@@ -0,0 +1,147 @@
+{
+  "collection": "JCB Pro Tools",
+  "total_products": 25,
+  "products_extracted": 20,
+  "products": [
+    {
+      "title": "JCB Pro 18V Compact Combi Drill 75Nm, Brushless, 2-Speed, 23+3 Positions & 13mm Chuck, 2 x Pro 5.0Ah Lithium-ion Batteries & 4.5A Charger | JCB-18PCD-75-5",
+      "sku": "JCB-18PCD-75-5",
+      "type": "Combi Drill",
+      "product_url": "https://jcb-tools.co.uk/products/jcb-18v-brushless-combi-drill-75nm-kit-18pcd-75-5",
+      "image_url": "https://jcb-tools.com/cdn/shop/files/1767452365525-0.jpg?v=1778762364&width=1280"
+    },
+    {
+      "title": "JCB Pro 18v Kit including 75Nm Combi Drill, 230Nm Impact Driver, 2x 2Ah Li-ion Batteries and 4.5A Fast Charger, with Carry Case | JCB-18PKIT-1",
+      "sku": "JCB-18PKIT-1",
+      "type": "Power Tool Kits",
+      "product_url": "https://jcb-tools.co.uk/products/jcb-18v-combi-drill-impact-driver-twin-pack-2ah-18pkit-1",
+      "image_url": "https://jcb-tools.com/cdn/shop/files/1767452214421-0.jpg?v=1778844216&width=1280"
+    },
+    {
+      "title": "JCB Pro 18V Kit, 75Nm Combi Drill, Impact Driver, 2x 5.0Ah Batteries & 4.5A Fast Charger (in Carry Case) | JCB-18PKIT-2",
+      "sku": "JCB-18PKIT-2",
+      "type": "Power Tool Kits",
+      "product_url": "https://jcb-tools.co.uk/products/jcb-18v-combi-drill-impact-driver-twin-pack-5ah-18pkit-2",
+      "image_url": "https://jcb-tools.com/cdn/shop/files/1767452250824-0.jpg?v=1778844319&width=1280"
+    },
+    {
+      "title": "JCB Pro 18V Kit, 160Nm Combi Drill, 230Nm Impact Driver, 2x 5.0Ah Batteries & 4.5A Fast Charger (in Carry Case) | JCB-18PKIT-3",
+      "sku": "JCB-18PKIT-3",
+      "type": "Power Tool Kits",
+      "product_url": "https://jcb-tools.co.uk/products/jcb-18v-160nm-combi-drill-impact-driver-twin-pack-18pkit-3",
+      "image_url": "https://jcb-tools.com/cdn/shop/files/1767452232954-0.jpg?v=1778844787&width=1280"
+    },
+    {
+      "title": "JCB Pro 18V Pro, 75Nm Combi Drill, 115mm Angle Grinder, 2x 5.0Ah Li-ion Batteries & 4.5A Fast Charger (with Soft Tool Bag) | JCB-18PKIT-4",
+      "sku": "JCB-18PKIT-4",
+      "type": "Power Tool Kits",
+      "product_url": "https://jcb-tools.co.uk/products/jcb-18v-combi-drill-angle-grinder-twin-pack-18pkit-4",
+      "image_url": "https://jcb-tools.com/cdn/shop/files/1767903912314-0.jpg?v=1778844090&width=1280"
+    },
+    {
+      "title": "JCB Pro 18V Combi Drill 160Nm, Brushless, Anti-Kickback & Variable Speed, 25+3 Torque, 2x 5.0Ah Lithium-ion Batteries & 4.5A Charger | JCB-18PCD-160-5",
+      "sku": "JCB-18PCD-160-5",
+      "type": "Combi Drill",
+      "product_url": "https://jcb-tools.co.uk/products/jcb-18v-160nm-brushless-combi-drill-kit-18pcd-160-5",
+      "image_url": "https://jcb-tools.com/cdn/shop/files/1767452394190-0.jpg?v=1778077371&width=1280"
+    },
+    {
+      "title": "JCB Pro 18V Kit, Combi Drill (75Nm), Impact Driver (230Nm), Multi Tool, 2Ah & 5Ah Batteries, 4.5A Fast Charger & Soft Tool Bag | JCB-18PKIT-6",
+      "sku": "JCB-18PKIT-6",
+      "type": "Power Tool Kits",
+      "product_url": "https://jcb-tools.co.uk/products/jcb-18v-combi-drill-impact-driver-multi-tool-kit-18pkit-6",
+      "image_url": "https://jcb-tools.com/cdn/shop/files/1767452282615-0.jpg?v=1778843913&width=1280"
+    },
+    {
+      "title": "JCB Pro 18V Kit, Combi Drill (75Nm), Impact Driver (230Nm), Angle Grinder (115mm), 2x 5Ah Batteries, 4.5A Fast Charger & Soft Tool Bag | JCB-18PKIT-7",
+      "sku": "JCB-18PKIT-7",
+      "type": "Power Tool Kits",
+      "product_url": "https://jcb-tools.co.uk/products/jcb-18v-combi-drill-impact-driver-circular-saw-kit-18pkit-7",
+      "image_url": "https://jcb-tools.com/cdn/shop/files/1767452302819-0.jpg?v=1774531458&width=1280"
+    },
+    {
+      "title": "JCB Pro 18V 5-Piece Pro Kit, Combi Drill (160Nm), Impact Driver (230Nm), Circular Saw (185mm), Angle Grinder (115mm), Jigsaw, 2x2Ah & 2x5Ah Batteries, Dual 4.5A Fast Charger & Soft Tool Bag | JCB-18PKIT-8",
+      "sku": "JCB-18PKIT-8",
+      "type": "Power Tool Kits",
+      "product_url": "https://jcb-tools.co.uk/products/jcb-18v-combi-drill-impact-driver-saw-grinder-kit-18pkit-8",
+      "image_url": "https://jcb-tools.com/cdn/shop/files/1767452325724-0.jpg?v=1781537319&width=1280"
+    },
+    {
+      "title": "JCB Pro 18V Kit ½” Impact Wrench, Grease Gun, 2x 5.0Ah Batteries & 4.5A Fast Charger (with Soft Tool Bag) | JCB-18PKIT-5",
+      "sku": "JCB-18PKIT-5",
+      "type": "Power Tool Kits",
+      "product_url": "https://jcb-tools.co.uk/products/jcb-18v-impact-wrench-grease-gun-twin-pack-18pkit-5",
+      "image_url": "https://jcb-tools.com/cdn/shop/files/1767452266016-0.jpg?v=1778844905&width=1280"
+    },
+    {
+      "title": "JCB Pro 18V Compact Impact Driver 230Nm, 4 Settings, Quick Release ¼” Chuck (Bare Tool) | JCB-18PID-230",
+      "sku": "JCB-18PID-230",
+      "type": "Impact Drivers",
+      "product_url": "https://jcb-tools.co.uk/products/jcb-18v-brushless-impact-driver-230nm-18pid-230",
+      "image_url": "https://jcb-tools.com/cdn/shop/files/1767452054651-0.jpg?v=1781469257&width=1280"
+    },
+    {
+      "title": "JCB Pro 18V Jigsaw, 135mm Cut, Brushless Motor, 4-Stage Pendulum Action (Bare Tool) | JCB-18PJS-DG",
+      "sku": "JCB-18PJS-DG",
+      "type": "Jigsaw",
+      "product_url": "https://jcb-tools.co.uk/products/jcb-18v-brushless-jigsaw-dust-guide-18pjs-dg",
+      "image_url": "https://jcb-tools.com/cdn/shop/files/1767452168788-0.jpg?v=1774455501&width=1280"
+    },
+    {
+      "title": "JCB Pro 18V Grease Gun Kit with 5.0Ah Battery & 9A Super Fast Charger | JCB-18PGG-VS-5",
+      "sku": "JCB-18PGG-VS-5",
+      "type": "Grease Gun",
+      "product_url": "https://jcb-tools.co.uk/products/jcb-18v-cordless-grease-gun-5ah-kit-18pgg-vs-5",
+      "image_url": "https://jcb-tools.com/cdn/shop/files/1767452464803-0.jpg?v=1778158288&width=1280"
+    },
+    {
+      "title": "JCB Pro 18V ½” Impact Wrench, 280Nm Fastening, 350Nm ‘Nut-Busting’ Torque, (Bare Tool) | JCB-18PIW-350",
+      "sku": "JCB-18PIW-350",
+      "type": "Impact Wrench",
+      "product_url": "https://jcb-tools.co.uk/products/jcb-18v-brushless-impact-wrench-350nm-18piw-350",
+      "image_url": "https://jcb-tools.com/cdn/shop/files/1767452073656-0.jpg?v=1778158558&width=1280"
+    },
+    {
+      "title": "JCB Pro 18V Compact Combi Drill 75Nm, Brushless, 2-Speed, 23+3 Positions & 13mm Metal Chuck (Bare Tool) | JCB-18PCD-75",
+      "sku": "JCB-18PCD-75",
+      "type": "Combi Drill",
+      "product_url": "https://jcb-tools.co.uk/products/jcb-18v-brushless-combi-drill-75nm-18pcd-75",
+      "image_url": "https://jcb-tools.com/cdn/shop/files/1767452017569-0.jpg?v=1781300770&width=1280"
+    },
+    {
+      "title": "JCB Pro 18V Multi Tool, Brushless, 9-Speed Control, Tool-Free Change (Bare Tool) | JCB-18PMT-32",
+      "sku": "JCB-18PMT-32",
+      "type": "Multi Tools",
+      "product_url": "https://jcb-tools.co.uk/products/jcb-18v-brushless-multi-tool-18pmt-32",
+      "image_url": "https://jcb-tools.com/cdn/shop/files/1767452133643-0.jpg?v=1774455326&width=1280"
+    },
+    {
+      "title": "JCB Pro 18V Circular Saw 185mm, 60mm Cut Depth, Aluminium Base, Incl. 2 Blades (Bare Tool) | JCB-18PCS-185",
+      "sku": "JCB-18PCS-185",
+      "type": "Circular Saw",
+      "product_url": "https://jcb-tools.co.uk/products/jcb-18v-185mm-circular-saw-18pcs-185",
+      "image_url": "https://jcb-tools.com/cdn/shop/files/1767452149319-0.jpg?v=1781328382&width=1280"
+    },
+    {
+      "title": "JCB Pro 18V Angle Grinder, 4.5\"/115mm, Digital Speed Control, Anti-Vibration, Paddle Switch (Bare Tool) | JCB-18PAG-115",
+      "sku": "JCB-18PAG-115",
+      "type": "Angle Grinders",
+      "product_url": "https://jcb-tools.co.uk/products/jcb-18v-115mm-brushless-angle-grinder-18pag-115",
+      "image_url": "https://jcb-tools.com/cdn/shop/files/1767451966697-0_e8553e05-a475-4faf-aa97-382e450c45de.jpg?v=1778074581&width=1280"
+    },
+    {
+      "title": "JCB Pro 18V Combi Drill 160Nm, Brushless, Anti-Kickback & Variable Speed, 25+3 Torque (Bare Tool) | JCB-18PCD-160",
+      "sku": "JCB-18PCD-160",
+      "type": "Combi Drill",
+      "product_url": "https://jcb-tools.co.uk/products/jcb-18v-brushless-combi-drill-160nm-18pcd-160",
+      "image_url": "https://jcb-tools.com/cdn/shop/files/1767451988349-0.jpg?v=1781684676&width=1280"
+    },
+    {
+      "title": "JCB Pro 18V Die Grinder, 6mm (1/4\") & 8mm (5/16\") Collet, Paddle Switch, Variable Speed Brushless (Bare Tool) | JCB-18PDG-6",
+      "sku": "JCB-18PDG-6",
+      "type": "Angle Grinders",
+      "product_url": "https://jcb-tools.co.uk/products/jcb-18v-brushless-die-grinder-6mm-18pdg-6",
+      "image_url": "https://jcb-tools.com/cdn/shop/files/1767452123337-0.jpg?v=1774455477&width=1280"
+    }
+  ]
+}
