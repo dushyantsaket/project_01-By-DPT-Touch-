4165ff605e2bbf8c37758e0df613915e4b26297b
Dushyant Saket <168100706+dushyantsaket@users.noreply.github.com>
2026-06-24 11:50:15 +0530

    Create newCollection.js

---
+++ OneDrive/New folder (2)/dushyant power tools/data/Air Impact 1/newCollection.js
@@ -0,0 +1,142 @@
+[
+  {
+    "title": "JCB 250 x 300mm Professional Square Mouth Site Shovel, Heavy-Duty Steel | JCBSS2S01",
+    "sku": "JCBSS2S01",
+    "type": "Shovels",
+    "product_url": "https://jcb-tools.co.uk/products/jcb-250-x-300mm-professional-square-mouth-site-shovel-heavy-duty-steel-jcbss2s01",
+    "image_url": "https://jcb-tools.com/cdn/shop/files/1767448033535-0_7c1c9410-340f-4811-a916-863e94406879.jpg?v=1774462295&width=1280"
+  },
+  {
+    "title": "JCB 100mm /4” Heavy-duty Petrol Wood Chipper 7.5hp, 224cc 4-Stroke | JCB-CH75100P",
+    "sku": "JCB-CH75100P",
+    "type": "Petrol Wood Chippers",
+    "product_url": "https://jcb-tools.co.uk/products/jcb-100mm-4-heavy-duty-petrol-wood-chipper-7-5hp-224cc-4-stroke-jcb-ch75100p",
+    "image_url": "https://jcb-tools.com/cdn/shop/files/1767449068768-0_a4773a62-7e4b-4ef4-aa8b-2be694624fab.jpg?v=1774461701&width=1280"
+  },
+  {
+    "title": "JCB 12 Teeth Garden Rake, 150cm x 30cm Head, Tubular Steel | JCBGR01",
+    "sku": "JCBGR01",
+    "type": "Rakes",
+    "product_url": "https://jcb-tools.co.uk/products/jcb-12-teeth-garden-rake-150cm-x-30cm-head-tubular-steel-jcbgr01",
+    "image_url": "https://jcb-tools.com/cdn/shop/files/1767449296101-0_d080a621-381b-4221-bb75-250a95fa6a9f.jpg?v=1774461592&width=1280"
+  },
+  {
+    "title": "JCB 120mm / 4.72” Heavy-duty Petrol Wood Chipper 457cc, 15hp 4-Stroke, Electric Start | JCB-CH150120PE",
+    "sku": "JCB-CH150120PE",
+    "type": "Petrol Wood Chippers",
+    "product_url": "https://jcb-tools.co.uk/products/jcb-120mm-4-72-heavy-duty-petrol-wood-chipper-457cc-15hp-4-stroke-electric-start-jcb-ch150120pe",
+    "image_url": "https://jcb-tools.com/cdn/shop/files/1767449082873-0.jpg?v=1774461435&width=1280"
+  },
+  {
+    "title": "JCB 130 x 150 mm Heritage Garden Hoe, Carbon Steel Blade, Ash Handle | JCBHDUHO01",
+    "sku": "JCBHDUHO01",
+    "type": "Spades",
+    "product_url": "https://jcb-tools.co.uk/products/jcb-130-x-150-mm-heritage-garden-hoe-carbon-steel-blade-ash-handle-jcbhduho01",
+    "image_url": "https://jcb-tools.com/cdn/shop/files/1767449508191-0_43fb2a16-d496-4573-b07c-6f4e6c14982e.jpg?v=1774461640&width=1280"
+  },
+  {
+    "title": "JCB 14’’ (32cm) Multitask Camping Axe, Ergonomic TPR Grip, Lightweight, Fiberglass Handle, Protective Carrying Sheath | JCBAXE01",
+    "sku": "JCBAXE01",
+    "type": "Axes",
+    "product_url": "https://jcb-tools.co.uk/products/jcb-14-32cm-multitask-camping-axe-ergonomic-tpr-grip-lightweight-fiberglass-handle-protective-carrying-sheath-jcbaxe01",
+    "image_url": "https://jcb-tools.com/cdn/shop/files/1767449818402-0_9e263fde-5b94-4fd4-9b91-49b399b835a6.jpg?v=1774461597&width=1280"
+  },
+  {
+    "title": "JCB 150cm Tubular Steel Dutch Hoe, 2.4kg, 15 x 13cm Head | JCBDUHO01",
+    "sku": "JCBDUHO01",
+    "type": "Augers",
+    "product_url": "https://jcb-tools.co.uk/products/jcb-150cm-tubular-steel-dutch-hoe-2-4kg-15-x-13cm-head-jcbduho01",
+    "image_url": "https://jcb-tools.com/cdn/shop/files/1767449286264-0.jpg?v=1774290552&width=1280"
+  },
+  {
+    "title": "JCB 180mm x 280mm Heritage Garden Spade, Heavy-Duty Steel Blade, Ash Wood Split YD Handle | JCBHGS01",
+    "sku": "JCBHGS01",
+    "type": "Spades",
+    "product_url": "https://jcb-tools.co.uk/products/jcb-120-x-250mm-heritage-garden-spade-heavy-duty-steel-blade-ash-wood-split-yd-handle-jcbhgs01",
+    "image_url": "https://jcb-tools.com/cdn/shop/files/1767447963592-0_65759351-a275-4106-aa53-f11f1a86ad29.jpg?v=1774461447&width=1280"
+  },
+  {
+    "title": "JCB 20 Teeth Heritage Lawn Rake 520mm Carbon Steel and Ash Handle | JCBHLR01",
+    "sku": "JCBHLR01",
+    "type": "Rakes",
+    "product_url": "https://jcb-tools.co.uk/products/jcb-20-teeth-heritage-lawn-rake-520mm-carbon-steel-and-ash-handle-jcbhlr01",
+    "image_url": "https://jcb-tools.com/cdn/shop/files/1767449500499-0.jpg?v=1774290628&width=1280"
+  },
+  {
+    "title": "JCB 200 x 100 mm Heritage Border Spade, Heavy-Duty Carbon Steel Blade, Ash Wood Split YD Handle | JCBHBS01",
+    "sku": "JCBHBS01",
+    "type": "Spades",
+    "product_url": "https://jcb-tools.co.uk/products/jcb-200-x-100-mm-heritage-border-spade-heavy-duty-carbon-steel-blade-ash-wood-split-yd-handle-jcbhbs01",
+    "image_url": "https://jcb-tools.com/cdn/shop/files/1767448017248-0.jpg?v=1774461421&width=1280"
+  },
+  {
+    "title": "JCB 230-210 x 305mm Blade Taper Mouth Site Shovel, Heavy-Duty Professional Steel Blade | JCBSS2T01",
+    "sku": "JCBSS2T11",
+    "type": "Shovels",
+    "product_url": "https://jcb-tools.co.uk/products/jcb-230-210-x-305mm-blade-taper-mouth-site-shovel-heavy-duty-professional-steel-blade-jcbss2t01",
+    "image_url": "https://jcb-tools.com/cdn/shop/files/1767447997826-0.jpg?v=1781706666&width=1280"
+  },
+  {
+    "title": "JCB 230-210 x 305mm Professional Tapered Mouth Site Master Shovel, Heavy-Duty Steel Blade | JCBSM2T01",
+    "sku": "JCBSM2T01",
+    "type": "Shovels",
+    "product_url": "https://jcb-tools.co.uk/products/jcb-230-210-x-305mm-professional-tapered-mouth-site-master-shovel-heavy-duty-steel-blade-jcbsm2t01",
+    "image_url": "https://jcb-tools.com/cdn/shop/files/1767448046592-0_6d5c9b53-05ce-4133-8c58-02c1d8f37ece.jpg?v=1774461735&width=1280"
+  },
+  {
+    "title": "JCB 250 x 300mm Square Mouth Site Master Shovel, Heavy-Duty Steel Blade | JCBSM2S11",
+    "sku": "JCBSM2S11",
+    "type": "Shovels",
+    "product_url": "https://jcb-tools.co.uk/products/jcb-250-x-300mm-square-mouth-site-master-shovel-heavy-duty-steel-blade-jcbsm2s11",
+    "image_url": "https://jcb-tools.com/cdn/shop/files/1767448079839-0.jpg?v=1774455499&width=1280"
+  },
+  {
+    "title": "JCB 250 x 320mm Square Open Socket Yard Shovel, Carbon Steel Forged Blade | JCBYS01",
+    "sku": "JCBYS01",
+    "type": "Shovels",
+    "product_url": "https://jcb-tools.co.uk/products/jcb-250-x-320mm-square-open-socket-yard-shovel-carbon-steel-forged-blade-jcbys01",
+    "image_url": "https://jcb-tools.com/cdn/shop/files/1767447969383-0_1ccefb8e-e320-4ee0-b441-12eb0cdd5cbd.jpg?v=1774461458&width=1280"
+  },
+  {
+    "title": "JCB 32'' 4 Prong Professional Manure Fork T Handle Heavy-Duty Steel | JCBMF11",
+    "sku": "JCBMF11",
+    "type": "Forks",
+    "product_url": "https://jcb-tools.co.uk/products/jcb-32-4-prong-professional-manure-fork-t-handle-heavy-duty-steel-jcbmf11",
+    "image_url": "https://jcb-tools.com/cdn/shop/files/1767448102829-0.jpg?v=1774290528&width=1280"
+  },
+  {
+    "title": "JCB 4 Prong Carbon Steel Heritage Garden Fork | JCBHGF01",
+    "sku": "JCBHGF01",
+    "type": "Forks",
+    "product_url": "https://jcb-tools.co.uk/products/jcb-4-prong-carbon-steel-heritage-garden-fork-jcbhgf01",
+    "image_url": "https://jcb-tools.com/cdn/shop/files/1767447974462-0_ff6774f4-1ab3-4db4-a7fe-0a305c1eb018.jpg?v=1774461426&width=1280"
+  },
+  {
+    "title": "JCB 400 x 180 /110mm Blade Grafting Spade (Newcastle Style) Drain Master, Professional Solid Forged | JCBDM01",
+    "sku": "JCBDM01",
+    "type": "Spades",
+    "product_url": "https://jcb-tools.co.uk/products/jcb-400-x-180-110mm-blade-grafting-spade-newcastle-style-drain-master-professional-solid-forged-jcbdm01",
+    "image_url": "https://jcb-tools.com/cdn/shop/files/1767447986150-0.jpg?v=1780058171&width=1280"
+  },
+  {
+    "title": "JCB 50mm 2’’ / 50mm Professional Petrol Water Pump, 466L/min, 7.5hp, 224cc, 4-Stroke | JCB-WP50",
+    "sku": "JCB-WP50",
+    "type": "Clean Water Pumps",
+    "product_url": "https://jcb-tools.co.uk/products/jcb-50mm-2-50mm-professional-petrol-water-pump-466l-min-7-5hp-224cc-4-stroke-jcb-wp50",
+    "image_url": "https://jcb-tools.com/cdn/shop/files/1767448796931-0_35853ab4-cc2e-4c60-b5a4-26014dd631e2.jpg?v=1774461446&width=1280"
+  },
+  {
+    "title": "JCB 7lb Chisel and Point Pick Axe - Professional Construction Tools | JCBPA11",
+    "sku": "JCBPA11",
+    "type": "Axes",
+    "product_url": "https://jcb-tools.co.uk/products/jcb-7lb-chisel-and-point-pick-axe-professional-construction-tools-jcbpa11",
+    "image_url": "https://jcb-tools.com/cdn/shop/files/1767449271800-3.jpg?v=1774290560&width=1280"
+  },
+  {
+    "title": "JCB 80mm 3’’ / 75mm Professional Petrol Trash / Dirty Water Pump, 1000L/min, 7.5hp, 224cc, 4-Stroke | JCB-WP80T",
+    "sku": "JCB-WP80T",
+    "type": "Trash Water Pumps",
+    "product_url": "https://jcb-tools.co.uk/products/jcb-80mm-3-75mm-professional-petrol-trash-dirty-water-pump-1000l-min-7-5hp-224cc-4-stroke-jcb-wp80t",
+    "image_url": "https://jcb-tools.com/cdn/shop/files/1767448828053-0.jpg?v=1774455120&width=1280"
+  }
+]
