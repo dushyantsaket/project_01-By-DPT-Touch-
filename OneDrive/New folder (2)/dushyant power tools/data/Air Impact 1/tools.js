14cd3ba77d3c9e423699266d7c1ca6b59b0e04a2
Dushyant Saket <168100706+dushyantsaket@users.noreply.github.com>
2026-07-18 21:13:10 +0530

    Create tools.js

---
+++ OneDrive/New folder (2)/dushyant power tools/data/Air Impact 1/tools.js
@@ -0,0 +1,128 @@
+{
+  "products": [
+    {
+      "id": 1,
+      "name": "IBELL Professional Tool Kit with Impact Drill TD13-85",
+      "price": 3233,
+      "currency": "INR",
+      "seller": "Amazon.in",
+      "other_sellers": true,
+      "rating": 5.0,
+      "reviews_count": 7,
+      "delivery_info": "Free delivery",
+      "product_url": null
+    },
+    {
+      "id": 2,
+      "name": "Bosch GDS 18V-350 Professional Cordless Impact Wrench",
+      "price": 10940,
+      "currency": "INR",
+      "seller": "Amazon.in",
+      "other_sellers": true,
+      "rating": 4.8,
+      "reviews_count": 474,
+      "delivery_info": "Free delivery",
+      "product_url": null
+    },
+    {
+      "id": 3,
+      "name": "Bosch GSR 120 Li Cordless Drill Driver",
+      "price": 5085,
+      "currency": "INR",
+      "seller": "Bosch Professional",
+      "other_sellers": true,
+      "rating": 4.8,
+      "reviews_count": 1900,
+      "delivery_info": "Free delivery",
+      "product_url": null
+    },
+    {
+      "id": 4,
+      "name": "THOR 48V 4-Tool Power Tool Combo Kit | 10mm Drill, Angle Grinder, Impact Wrench, Rotary Hammer | 3x5Ah Batteries, Fast Charger, Brushless Motors",
+      "price": 11599,
+      "currency": "INR",
+      "seller": "Amazon.in",
+      "other_sellers": false,
+      "rating": null,
+      "reviews_count": null,
+      "delivery_info": "Free delivery",
+      "product_url": null
+    },
+    {
+      "id": 5,
+      "name": "iBELL CT20-38 Cordless Impact Drill Tool Kit",
+      "price": 5848,
+      "currency": "INR",
+      "seller": "ibelltools",
+      "other_sellers": false,
+      "rating": null,
+      "reviews_count": null,
+      "delivery_info": "Free delivery",
+      "product_url": null
+    },
+    {
+      "id": 6,
+      "name": "Bosch GSB 600 Corded Electric Impact Drill",
+      "price": 5199,
+      "currency": "INR",
+      "seller": "Amazon.in",
+      "other_sellers": true,
+      "rating": 4.8,
+      "reviews_count": 45,
+      "delivery_info": "Free delivery",
+      "product_url": null
+    },
+    {
+      "id": 7,
+      "name": "Total Impact Drill TG109136",
+      "price": 4212,
+      "currency": "INR",
+      "seller": "Total Tools India",
+      "other_sellers": true,
+      "rating": 4.8,
+      "reviews_count": 16,
+      "delivery_info": "Free delivery by Thu",
+      "product_url": null
+    },
+    {
+      "id": 8,
+      "name": "Ingco 20V Cordless Impact Wrench 405NM, Variable Speed 2400RPM, With 1Pcs Work Lamp, 2Pcs 2.0Ah Battery And Charger, 10Pcs Socket, 10pcs Screwdriver",
+      "price": 6509,
+      "currency": "INR",
+      "seller": "Amazon.in",
+      "other_sellers": false,
+      "rating": 4.7,
+      "reviews_count": 12,
+      "delivery_info": "Free delivery",
+      "product_url": null
+    },
+    {
+      "id": 9,
+      "name": "Bosch GSB 10 RE Professional Impact Drill",
+      "price": 4279.86,
+      "currency": "INR",
+      "seller": "ToolBuy.com",
+      "other_sellers": true,
+      "rating": null,
+      "reviews_count": null,
+      "delivery_info": "Free delivery",
+      "product_url": null
+    },
+    {
+      "id": 10,
+      "name": "Izom IZ-CDK-20V Cordless Drill",
+      "price": 3499,
+      "currency": "INR",
+      "seller": "Amazon.in",
+      "other_sellers": false,
+      "rating": null,
+      "reviews_count": null,
+      "delivery_info": "Free delivery",
+      "product_url": null
+    }
+  ],
+  "total_count": 10,
+  "currency": "INR",
+  "source": "Google Shopping (India)",
+  "retrieved_date": "2026-07-18"
+}
