17da5d4d694a77aa97c82aa46bcc62aab8ab8a19
Dushyant Saket <168100706+dushyantsaket@users.noreply.github.com>
2026-07-22 08:06:55 +0530

    Create tataHandTools.js

---
+++ OneDrive/New folder (2)/dushyant power tools/data/Air Impact 1/tataHandTools.js
@@ -0,0 +1,427 @@
+{
+  "products": [
+    {
+      "id": "wra001",
+      "name": "TATA AGRICO Adjustable Wrench 8 inches – WRA001",
+      "slug": "tata-agrico-adjustable-wrench-8-inches-wra001",
+      "image": "/wp-content/uploads/2025/05/1-5-300x300.jpg",
+      "price": "₹499",
+      "dealer_price": {
+        "Flipkart": "₹479",
+        "Amazon": "₹510"
+      },
+      "retailers": [
+        {
+          "name": "Flipkart",
+          "url": "https://www.flipkart.com/tata-agrico-wra001-8-200mm-single-sided-open-end-wrench/p/itm26e163ef577dc",
+          "price": "₹479"
+        },
+        {
+          "name": "Amazon",
+          "url": "https://www.amazon.in/dp/B01LXONSK7",
+          "price": "₹510"
+        }
+      ]
+    },
+    {
+      "id": "wrs003_new",
+      "name": "TATA AGRICO Pipe Wrench (12 Inch/300 mm), Multicolour – WRS003_NEW",
+      "slug": "tata-agrico-pipe-wrench-12-inch-300-mm-multicolour-wrs003_new",
+      "image": "/wp-content/uploads/2025/05/1-7-300x300.jpg",
+      "price": "₹649",
+      "dealer_price": {
+        "Amazon": "₹629"
+      },
+      "retailers": [
+        {
+          "name": "Amazon",
+          "url": "https://www.amazon.in/dp/B07MNF5Q74",
+          "price": "₹629"
+        }
+      ]
+    },
+    {
+      "id": "wrs004_new",
+      "name": "TATA AGRICO Stilson Type Pipe Wrench | Ideal for Construction & Heavy-Duty Applications for Industrial & Professional Use | Alloy Steel Wrench GREY & BLUE | Size -14Inch/350 mm – WRS004_NEW",
+      "slug": "tata-agrico-stilson-type-pipe-wrench-ideal-for-construction-professional-use-alloy-steel-wrench-grey-blue-size-14inch-350-mm-wrs004_new",
+      "image": "/wp-content/uploads/2025/05/1-8-300x300.jpg",
+      "price": "₹749",
+      "dealer_price": {
+        "Amazon": "₹699"
+      },
+      "retailers": [
+        {
+          "name": "Amazon",
+          "url": "https://www.amazon.in/dp/B07MGR62C6",
+          "price": "₹699"
+        }
+      ]
+    },
+    {
+      "id": "wrs005_new",
+      "name": "TATA AGRICO Stilson Type Pipe Wrench Ideal for Construction & Heavy-Duty Applications for Industrial & Professional Use, Alloy Steel Wrench GREY & BLUE (18 inch/ 380mm) – WRS005_NEW",
+      "slug": "tata-agrico-stilson-type-pipe-wrench-ideal-for-construction-professional-use-alloy-steel-wrench-grey-blue-18-inch-380mm-wrs005_new",
+      "image": "/wp-content/uploads/2025/05/1-9-300x300.jpg",
+      "price": "₹899",
+      "dealer_price": {
+        "Amazon": "₹849"
+      },
+      "retailers": [
+        {
+          "name": "Amazon",
+          "url": "https://www.amazon.in/dp/B07M5T7MTQ",
+          "price": "₹849"
+        }
+      ]
+    },
+    {
+      "id": "wrc008",
+      "name": "TATA AGRICO Blister – Sturdy Adjustable wrench | 10″ Rubber Sleeve | For Home And Professional Use – WRC008",
+      "slug": "tata-agrico-blister-sturdy-adjustable-wrench-10-rubber-sleeve-for-home-and-professional-use-wrc008",
+      "image": "/wp-content/uploads/2025/05/1-6-300x300.jpg",
+      "price": "₹599",
+      "dealer_price": {
+        "Amazon": "₹559"
+      },
+      "retailers": [
+        {
+          "name": "Amazon",
+          "url": "https://www.amazon.in/dp/B07WM2HM7Z",
+          "price": "₹559"
+        }
+      ]
+    },
+    {
+      "id": "wrs002",
+      "name": "TATA AGRICO Single Sided Pipe Wrench (Pack of 1) 10″/250mm – WRS002",
+      "slug": "tata-agrico-single-sided-pipe-wrench-pack-of-1-10-250mm-wrs002",
+      "image": "/wp-content/uploads/2024/12/1-159-300x300.jpg",
+      "price": "₹699",
+      "dealer_price": {
+        "Aashiyana": "₹650",
+        "Flipkart": "₹680"
+      },
+      "retailers": [
+        {
+          "name": "Aashiyana",
+          "url": "https://aashiyana.tatasteel.com/in/en/buy-our-products/brands/tata-agrico/product-details/agrc-7",
+          "price": "₹650"
+        },
+        {
+          "name": "Flipkart",
+          "url": "https://www.flipkart.com/tata-agrico-wrs002-stillson-pipe-wrench-8-wrs001-single-sided-combination/p/itm85f916772df98?pid=WRSGVRBP7E7CGZGB&lid=LSTWRSGVRBP7E7CGZGBA6YZXA&marketplace=FLIPKART",
+          "price": "₹680"
+        }
+      ]
+    },
+    {
+      "id": "wrs005",
+      "name": "TATA AGRICO Single Sided Pipe Wrench (Pack of 1) 18inch/450mm – WRS005",
+      "slug": "tata-agrico-single-sided-pipe-wrench-pack-of-1-18inch-450mm-wrs005",
+      "image": "/wp-content/uploads/2024/12/1-162-300x300.jpg",
+      "price": "₹999",
+      "dealer_price": {
+        "Aashiyana": "₹920",
+        "Flipkart": "₹970",
+        "Amazon": "₹950"
+      },
+      "retailers": [
+        {
+          "name": "Aashiyana",
+          "url": "https://aashiyana.tatasteel.com/in/en/buy-our-products/brands/tata-agrico/product-details/agrc-15",
+          "price": "₹920"
+        },
+        {
+          "name": "Flipkart",
+          "url": "https://www.flipkart.com/tata-agrico-wrs005-18inch-450mm-single-sided-pipe-wrench/p/itmef403c4c56445",
+          "price": "₹970"
+        },
+        {
+          "name": "Amazon",
+          "url": "https://www.amazon.in/TATA-AGRICO-Heavy-Duty-Wrench/dp/B07G6KWCSL",
+          "price": "₹950"
+        }
+      ]
+    },
+    {
+      "id": "wrs004",
+      "name": "TATA AGRICO Single Sided Pipe Wrench (Pack of 1) 14inch/355mm – WRS004",
+      "slug": "tata-agrico-single-sided-pipe-wrench-pack-of-1-14inch-355mm-wrs004",
+      "image": "/wp-content/uploads/2024/12/1-161-300x300.jpg",
+      "price": "₹849",
+      "dealer_price": {
+        "Aashiyana": "₹790",
+        "Flipkart": "₹820",
+        "Amazon": "₹810"
+      },
+      "retailers": [
+        {
+          "name": "Aashiyana",
+          "url": "https://aashiyana.tatasteel.com/in/en/buy-our-products/brands/tata-agrico/product-details/agrc-8",
+          "price": "₹790"
+        },
+        {
+          "name": "Flipkart",
+          "url": "https://www.flipkart.com/tata-agrico-wrs004-single-sided-pipe-wrench/p/itmeb0b1b13d2a15",
+          "price": "₹820"
+        },
+        {
+          "name": "Amazon",
+          "url": "https://www.amazon.in/dp/B07MGR62C6",
+          "price": "₹810"
+        }
+      ]
+    },
+    {
+      "id": "wrs003",
+      "name": "TATA AGRICO Single Sided Pipe Wrench (Pack of 1) 12inch/300mm – WRS003",
+      "slug": "tata-agrico-single-sided-pipe-wrench-pack-of-1-12inch-300mm-wrs003",
+      "image": "/wp-content/uploads/2024/12/1-160-300x300.jpg",
+      "price": "₹749",
+      "dealer_price": {
+        "Aashiyana": "₹700",
+        "Flipkart": "₹730",
+        "Amazon": "₹720"
+      },
+      "retailers": [
+        {
+          "name": "Aashiyana",
+          "url": "https://aashiyana.tatasteel.com/in/en/buy-our-products/brands/tata-agrico/product-details/agrc-6",
+          "price": "₹700"
+        },
+        {
+          "name": "Flipkart",
+          "url": "https://www.flipkart.com/tata-agrico-wrs003-12inch-300mm-single-sided-pipe-wrench/p/itmd87724a8d8e54",
+          "price": "₹730"
+        },
+        {
+          "name": "Amazon",
+          "url": "https://www.amazon.in/Tata-Agrico-Pipe-Wrench-Multicolour/dp/B07MNF5Q74?th=1",
+          "price": "₹720"
+        }
+      ]
+    },
+    {
+      "id": "wrs001",
+      "name": "TATA AGRICO Single Sided Pipe Wrench (Pack of 1) – WRS001",
+      "slug": "tata-agrico-single-sided-pipe-wrench-pack-of-1-wrs001",
+      "image": "/wp-content/uploads/2024/12/1-158-300x300.jpg",
+      "price": "₹599",
+      "dealer_price": {
+        "Aashiyana": "₹550",
+        "Flipkart": "₹580",
+        "Amazon": "₹570"
+      },
+      "retailers": [
+        {
+          "name": "Aashiyana",
+          "url": "https://aashiyana.tatasteel.com/in/en/buy-our-products/brands/tata-agrico/product-details/agrc-3",
+          "price": "₹550"
+        },
+        {
+          "name": "Flipkart",
+          "url": "https://www.flipkart.com/tata-agrico-wrs001-single-sided-pipe-wrench/p/itm824d825e00148",
+          "price": "₹580"
+        },
+        {
+          "name": "Amazon",
+          "url": "https://www.amazon.in/Tata-Agrico-Pipe-Wrench/dp/B07MGR62C6?th=1",
+          "price": "₹570"
+        }
+      ]
+    },
+    {
+      "id": "wrc006",
+      "name": "TATA AGRICO Single Sided Adjustable Wrench 640 mm, 150 mm – WRC006",
+      "slug": "tata-agrico-single-sided-adjustable-wrench-640-mm-150-mm-wrc006",
+      "image": "/wp-content/uploads/2024/12/1-157-300x300.jpg",
+      "price": "₹899",
+      "badge": "NEW",
+      "dealer_price": {
+        "Flipkart": "₹850",
+        "Amazon": "₹880"
+      },
+      "retailers": [
+        {
+          "name": "Flipkart",
+          "url": "https://www.flipkart.com/agrico-wrc006-640-mm-150-mm-single-sided-adjustable-wrench/p/itm9215ad956a633",
+          "price": "₹850"
+        },
+        {
+          "name": "Amazon",
+          "url": "https://www.amazon.in/dp/B07WHCXZC2",
+          "price": "₹880"
+        }
+      ]
+    },
+    {
+      "id": "wrc005",
+      "name": "TATA AGRICO Single Sided Combination Wrench (Pack of 1) – WRC005",
+      "slug": "tata-agrico-single-sided-combination-wrench-pack-of-1-wrc005",
+      "image": "/wp-content/uploads/2024/12/1-156-300x300.jpg",
+      "price": "₹699",
+      "badge": "NEW",
+      "dealer_price": {
+        "Flipkart": "₹660",
+        "Amazon": "₹680"
+      },
+      "retailers": [
+        {
+          "name": "Flipkart",
+          "url": "https://www.flipkart.com/tata-agrico-wrc005-single-sided-combination-wrench/p/itmd2537a6fa2563",
+          "price": "₹660"
+        },
+        {
+          "name": "Amazon",
+          "url": "https://www.amazon.in/dp/B07WJH82M8",
+          "price": "₹680"
+        }
+      ]
+    },
+    {
+      "id": "wrc003",
+      "name": "TATA AGRICO Single Sided Combination Wrench (Pack of 1) – WRC003",
+      "slug": "tata-agrico-single-sided-combination-wrench-pack-of-1-wrc003",
+      "image": "/wp-content/uploads/2024/12/1-155-300x300.jpg",
+      "price": "₹549",
+      "dealer_price": {
+        "Aashiyana": "₹520",
+        "Flipkart": "₹540",
+        "Amazon": "₹530"
+      },
+      "retailers": [
+        {
+          "name": "Aashiyana",
+          "url": "https://aashiyana.tatasteel.com/in/en/buy-our-products/brands/tata-agrico/product-details/agrc-81",
+          "price": "₹520"
+        },
+        {
+          "name": "Flipkart",
+          "url": "https://www.flipkart.com/agrico-wrc003-single-sided-combination-wrench/p/itm2348c97a14115?pid=WRSFZ43VCXVDHKYV&lid=LSTWRSFZ43VCXVDHKYVTDY2GK&marketplace=FLIPKART",
+          "price": "₹540"
+        },
+        {
+          "name": "Amazon",
+          "url": "https://www.amazon.in/dp/B07WF8BGHS?th=1",
+          "price": "₹530"
+        }
+      ]
+    },
+    {
+      "id": "wrc002",
+      "name": "TATA AGRICO Single Sided Open End Wrench (Pack of 1) – WRC002",
+      "slug": "tata-agrico-single-sided-open-end-wrench-pack-of-1-wrc002",
+      "image": "/wp-content/uploads/2024/12/1-154-300x300.jpg",
+      "price": "₹449",
+      "dealer_price": {
+        "Aashiyana": "₹420",
+        "Flipkart": "₹440",
+        "Amazon": "₹430"
+      },
+      "retailers": [
+        {
+          "name": "Aashiyana",
+          "url": "https://aashiyana.tatasteel.com/in/en/buy-our-products/brands/tata-agrico/product-details/agrc-86",
+          "price": "₹420"
+        },
+        {
+          "name": "Flipkart",
+          "url": "https://www.flipkart.com/agrico-wrc002-single-sided-open-end-wrench/p/itm29a0a39579e92?pid=WRSFZBP83YZ2BUTT&cmpid=product.share.pp&_refId=PP.aa6b12f0-e9a4-4d65-9049-2221198af31a.WRSFZBP83YZ2BUTT",
+          "price": "₹440"
+        },
+        {
+          "name": "Amazon",
+          "url": "https://www.amazon.in/dp/B07WHCWZF1?th=1",
+          "price": "₹430"
+        }
+      ]
+    },
+    {
+      "id": "wra001_old",
+      "name": "TATA AGRICO Single Sided Open End Wrench (Pack of 1) (8″/200mm) – WRA001",
+      "slug": "tata-agrico-single-sided-open-end-wrench-pack-of-1-8-200mm-wra001",
+      "image": "/wp-content/uploads/2024/12/1-151-300x300.jpg",
+      "price": "₹499",
+      "dealer_price": {
+        "Aashiyana": "₹460",
+        "Flipkart": "₹480",
+        "Amazon": "₹490"
+      },
+      "retailers": [
+        {
+          "name": "Aashiyana",
+          "url": "https://aashiyana.tatasteel.com/in/en/buy-our-products/brands/tata-agrico/product-details/agrc-71",
+          "price": "₹460"
+        },
+        {
+          "name": "Flipkart",
+          "url": "https://www.flipkart.com/tata-agrico-wra001-8-200mm-single-sided-open-end-wrench/p/itm26e163ef577dc",
+          "price": "₹480"
+        },
+        {
+          "name": "Amazon",
+          "url": "https://www.amazon.in/TATA-AGRICO-Chrome-Vanadium-Adjustable/dp/B01LXONSK7?th=1",
+          "price": "₹490"
+        }
+      ]
+    },
+    {
+      "id": "wra003",
+      "name": "TATA AGRICO Single Sided Speed Wrench (Pack of 1) 12″ – WRA003",
+      "slug": "tata-agrico-single-sided-speed-wrench-pack-of-1-12-wra003",
+      "image": "/wp-content/uploads/2024/12/1-153-300x300.jpg",
+      "price": "₹799",
+      "dealer_price": {
+        "Aashiyana": "₹750",
+        "Flipkart": "₹780",
+        "Amazon": "₹770"
+      },
+      "retailers": [
+        {
+          "name": "Aashiyana",
+          "url": "https://aashiyana.tatasteel.com/in/en/buy-our-products/brands/tata-agrico/product-details/agrc-4",
+          "price": "₹750"
+        },
+        {
+          "name": "Flipkart",
+          "url": "https://www.flipkart.com/tata-agrico-wra003-12-single-sided-speed-wrench/p/itmexf2ahyg99vdh",
+          "price": "₹780"
+        },
+        {
+          "name": "Amazon",
+          "url": "https://www.amazon.in/TATA-AGRICO-ADJUSTABLE-WRENCH-12/dp/B0CWXJCBLX",
+          "price": "₹770"
+        }
+      ]
+    },
+    {
+      "id": "wra002",
+      "name": "TATA AGRICO Wra Single Sided Open End Wrench (Pack of 1) – WRA002",
+      "slug": "tata-agrico-wra-single-sided-open-end-wrench-pack-of-1-wra002",
+      "image": "/wp-content/uploads/2024/12/1-152-300x300.jpg",
+      "price": "₹549",
+      "dealer_price": {
+        "Aashiyana": "₹510",
+        "Flipkart": "₹530",
+        "Amazon": "₹540"
+      },
+      "retailers": [
+        {
+          "name": "Aashiyana",
+          "url": "https://aashiyana.tatasteel.com/in/en/buy-our-products/brands/tata-agrico/product-details/agrc-5",
+          "price": "₹510"
+        },
+        {
+          "name": "Flipkart",
+          "url": "https://www.flipkart.com/tata-agrico-wra002-wra-single-sided-open-end-wrench/p/itmc908b105addf5?pid=WREERV5ZGWFCYUFU&lid=LSTWREERV5ZGWFCYUFUYMVHPH&marketplace=FLIPKART&cmpid=content_wrench-set_8965229628_gmc",
+          "price": "₹530"
+        },
+        {
+          "name": "Amazon",
+          "url": "https://www.amazon.in/dp/B01LY0FI6X?th=1",
+          "price": "₹540"
+        }
+      ]
+    }
+  ]
+}
