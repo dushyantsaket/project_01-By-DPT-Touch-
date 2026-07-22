d4df85a3d0c85e12bf78dbccb66be8c45d126db6
Dushyant Saket <168100706+dushyantsaket@users.noreply.github.com>
2026-07-22 08:13:39 +0530

    Create tataTools.js

---
+++ OneDrive/New folder (2)/dushyant power tools/data/Air Impact 1/tataTools.js
@@ -0,0 +1,39 @@
+{
+  "products": [
+    {
+      "name": "Tata Agrico Sp-08 Spanner Set Double Sided Speciality",
+      "image": "/images/spanner-set-1.jpg",
+      "offers": [
+        {
+          "retailer": "Flipkart",
+          "price": "₹447",
+          "delivery": "Free delivery"
+        }
+      ]
+    },
+    {
+      "name": "TATA Agrico 8-in-1 Screwdriver Set",
+      "image": "/images/screwdriver-set.jpg",
+      "offers": [
+        {
+          "retailer": "Amazon.in",
+          "price": "₹279",
+          "delivery": "Free delivery"
+        }
+      ]
+    },
+    {
+      "name": "Tata Agrico Spd015 D.O.E Spanner Set Double Sided Speciality",
+      "image": "/images/spanner-set-2.jpg",
+      "offers": [
+        {
+          "retailer": "Flipkart",
+          "price": "₹495",
+          "delivery": "Free delivery",
+          "originalPrice": "₹580",
+          "discount": "15% off"
+        }
+      ]
+    }
+  ]
+}
