ac2f597a13ef682d26e1babb08be5efce2e35b4b
Dushyant Saket <168100706+dushyantsaket@users.noreply.github.com>
2026-07-22 08:08:00 +0530

    Create tataToolas.js

---
+++ OneDrive/New folder (2)/dushyant power tools/data/Air Impact 1/tataToolas.js
@@ -0,0 +1,95 @@
+{
+  "category": "Agri & Construction Tools",
+  "subcategories": [
+    {
+      "id": "chaff-cutter-thresher-cutter",
+      "name": "Chaff Cutter & Thresher Cutter",
+      "image": "/wp-content/uploads/2025/08/chaff-cutter.png",
+      "link": "/product-category/agri-and-construction-tools/chaff-cutter-thresher-cutter/",
+      "variations": 3,
+      "price": 3,
+      "dealer_price": {}
+    },
+    {
+      "id": "tiller-shoe",
+      "name": "Tiller Shoe",
+      "image": "/wp-content/uploads/2025/08/Tiller-Shoe.png",
+      "link": "/product-category/agri-and-construction-tools/tiller-shoe/",
+      "variations": 13,
+      "price": 13,
+      "dealer_price": {}
+    },
+    {
+      "id": "total-rotavator-blade",
+      "name": "Total Rotavator Blade",
+      "image": "/wp-content/uploads/2025/08/Rotary-Tiller-Blade.png",
+      "link": "/product-category/agri-and-construction-tools/total-rotavator-blade/",
+      "variations": 6,
+      "price": 6,
+      "dealer_price": {}
+    },
+    {
+      "id": "chisel",
+      "name": "Chisel",
+      "image": "/wp-content/uploads/2024/12/1-6.jpg",
+      "link": "/product-category/agri-and-construction-tools/chisel/",
+      "variations": 15,
+      "price": 15,
+      "dealer_price": {}
+    },
+    {
+      "id": "crowbar",
+      "name": "Crowbar",
+      "image": "/wp-content/uploads/2024/12/Crowbar-category-image.jpg",
+      "link": "/product-category/agri-and-construction-tools/crowbar/",
+      "variations": 27,
+      "price": 27,
+      "dealer_price": {}
+    },
+    {
+      "id": "agri-hammer",
+      "name": "Hammer",
+      "image": "/wp-content/uploads/2024/12/1-59.jpg",
+      "link": "/product-category/agri-and-construction-tools/agri-hammer/",
+      "variations": 37,
+      "price": 37,
+      "dealer_price": {}
+    },
+    {
+      "id": "hoe",
+      "name": "Hoe",
+      "image": "/wp-content/uploads/2024/12/1-165.jpg",
+      "link": "/product-category/agri-and-construction-tools/hoe/",
+      "variations": 62,
+      "price": 62,
+      "dealer_price": {}
+    },
+    {
+      "id": "pickaxe",
+      "name": "Pickaxe",
+      "image": "/wp-content/uploads/2024/12/Pickaxe-Category-image.jpg",
+      "link": "/product-category/agri-and-construction-tools/pickaxe/",
+      "variations": 19,
+      "price": 19,
+      "dealer_price": {}
+    },
+    {
+      "id": "shovel",
+      "name": "Shovel",
+      "image": "/wp-content/uploads/2024/12/1-130.jpg",
+      "link": "/product-category/agri-and-construction-tools/shovel/",
+      "variations": 22,
+      "price": 22,
+      "dealer_price": {}
+    },
+    {
+      "id": "sickle",
+      "name": "Sickle",
+      "image": "/wp-content/uploads/2024/12/Sickle-category-image.jpg",
+      "link": "/product-category/agri-and-construction-tools/sickle/",
+      "variations": 37,
+      "price": 37,
+      "dealer_price": {}
+    }
+  ]
+}
