73dfd0cf683b4fd25dc14e99ea490d17772db287
Dushyant Saket <168100706+dushyantsaket@users.noreply.github.com>
2026-07-22 08:19:41 +0530

    Create tapriatools.js

---
+++ OneDrive/New folder (2)/dushyant power tools/data/Air Impact 1/tapriatools.js
@@ -0,0 +1,74 @@
+[
+  {
+    "id": 1,
+    "name": "Taparia Multipurpose Hand Tool Kit",
+    "customer_price": "₹705",
+    "dealer_price": "₹880",
+    "discount": "20% off",
+    "seller": "Flipkart",
+    "image_url": "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcT-aVgbIXmVsQPssieDePL5klr7y_uXtUFQne7X0PHj0nC2h97AslPKBzyxbkOHCHH_yOrHU2cICJ861FuXc5a7XfAYNJjoGHMG3_jECwCuFMVVdOFNiw8z"
+  },
+  {
+    "id": 2,
+    "name": "Taparia Impact Driver Screwdriver Set",
+    "customer_price": "₹1,400",
+    "dealer_price": null,
+    "discount": null,
+    "seller": "Amazon.in",
+    "image_url": "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSA9SZeN9CoFdHvNTG_5RitFwNMwWWY7XSYomefq2uc_68i0Twn-6_W4-3qHbRUaB_rNuxUpN7Co8NcGEVoVPZjtKYztESGOQwAQH0bFuasfKwBPs2Z5HWY"
+  },
+  {
+    "id": 3,
+    "name": "TAPARIA Hand Tool Kit",
+    "customer_price": "₹170",
+    "dealer_price": null,
+    "discount": null,
+    "seller": "shopsy.in",
+    "image_url": "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRANkw6bTZ6lKfT3TAKg193KHKl51IoJqFESULGuY2wbhCmO6mIzV2LDJ-9Scm21KML_sDGV8Xk5RP6TSaUQUujdHAhaf3aSbhJGmzaX2ZF6cK-lan4nl4ibw"
+  },
+  {
+    "id": 4,
+    "name": "Taparia 12 Pcs, 6 mm - 22 mm Combination Spanner Set, CSS 12",
+    "customer_price": "₹543.98",
+    "dealer_price": null,
+    "discount": null,
+    "seller": "ToolBuy.com",
+    "image_url": "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQjmS01YK5TDsyfuZDnPx_Wwv1TvDX1RKvY6lIUFC2gB0AZfZ-6uOw-gTE2WyoyJtcAbMRKPTIeKb-pfF4xc8LwmYQYe-cEQrQUgjTyBiiQokFwglIVnXse6w"
+  },
+  {
+    "id": 5,
+    "name": "Taparia Precision Screw Driver Set",
+    "customer_price": "₹302",
+    "dealer_price": null,
+    "discount": null,
+    "seller": "Amazon.in",
+    "image_url": "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRVkNn51DE85P5qBWY57HOJqcbNju12kO1HNkNsUDohSJjziaMd1-3qyVpNSAwwh_OwOXq6yOFRwlL-T0wbC7wGU_sbyTh9RGXuNgAjiAFXfZ9Q94lTcMRduA"
+  },
+  {
+    "id": 6,
+    "name": "Taparia For Selfwork / Home Purpose Hand Tool Kit",
+    "customer_price": "₹1,838",
+    "dealer_price": null,
+    "discount": null,
+    "seller": "Flipkart",
+    "image_url": "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQYWhlku1hlA5QTq-KlGmLB_h6Bzzp2nGat4D-j8ad6aKOYc80yEQW5X-cayphlk7TQBf50k_jCAdo7l7i-p5-53w6r3eQzS9zLx3wu6BWANba4BqtKswJWGg"
+  },
+  {
+    "id": 7,
+    "name": "Taparia Socket Set",
+    "customer_price": "₹4,956",
+    "dealer_price": null,
+    "discount": null,
+    "seller": "Tradeindia.com",
+    "image_url": "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSs5shXWeNq4f35-5MzextG8tO7QVj22VZzpT68J19ja-wkrA16XT3hn_tEQgjST0GO6Pj5CMej8gY41vmf4iRB16ZcPKtVWgDHN8gfRrjT"
+  },
+  {
+    "id": 8,
+    "name": "Taparia Tools Taparia Dep-12, Clh-450, Bs-31, 813 Screwdriver, Line Tester, Claw Hammer Set",
+    "customer_price": "₹2,035",
+    "dealer_price": "₹2,999",
+    "discount": "32% off",
+    "seller": "Flipkart",
+    "image_url": "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQr8AWr_7m7g-qhZzYky9QdiY1V02AAvEmlxqSwSgQ8bsMb9-vUb7ev_FeA97ig1qhWEVBJUnPjLC4jsyVjZdyCUdafHU_MddBYjrGE5hq-MIH5gnatdd4Pxw"
+  }
+]
